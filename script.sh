994a74d8ad931ee2ea76ba9bce2200b23a3d358a
aggalan <127793974+aggalan@users.noreply.github.com>
2024-05-23 12:49:10 -0300

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker start LOCAL
-docker exec -it LOCAL make clean -C/root/Toolchain
-docker exec -it LOCAL make clean -C/root/
-docker exec -it LOCAL make -C/root/Toolchain
-docker exec -it LOCAL make -C/root/
-docker stop LOCAL
